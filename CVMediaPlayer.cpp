d1112e84a04dc75f6ee1b2614d6b8062e7d6b4db
wammero <nazhmud1@yandex.ru>
2024-09-20 12:20:35 +0300

    Console CVMediaPlayer

---
+++ CVMediaPlayer.cpp
@@ -0,0 +1,80 @@
+#include <chrono>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+#include <string>
+#include <thread>
+
+#include <ncurses.h>
+
+const char get_ASCII_from_pixel(const int pixelintensity) {
+  std::string chars_by_brightness = "$@B%8&#*/|(-_+;:,.  ";
+  std::reverse(chars_by_brightness.begin(), chars_by_brightness.end());
+  return chars_by_brightness[static_cast<float>(pixelintensity *
+                                                chars_by_brightness.length()) /
+                             256.f];
+}
+
+int main() {
+  cv::VideoCapture video_capture("name.mp4"); // change name for your video 
+
+  if (!video_capture.isOpened()) {
+    std::cerr << "Failed to open video file.\n";
+    return -1;
+  }
+
+  const float correction_factor = 4.75;
+  const int targetfps = 10;
+  const int fps = video_capture.get(cv::CAP_PROP_FPS);
+  const int fpsdif = fps / targetfps;
+  const int frame_duration = 1000 / fps;
+  const int frame_width = video_capture.get(cv::CAP_PROP_FRAME_WIDTH);
+  const int frame_height = video_capture.get(cv::CAP_PROP_FRAME_HEIGHT);
+  const int screen_height = 70; // change this param for your screen
+  const int screen_width =
+      screen_height * (frame_width / frame_height) * correction_factor;
+
+  int starttime = 0, endtime = 0, framedrawtime = 0, i = 0;
+  cv::Mat original_frame, grayscaled_frame, grayscaled_resized_frame;
+
+  initscr();
+  for (;;) {
+    starttime = std::chrono::duration_cast<std::chrono::milliseconds>(
+                    std::chrono::system_clock::now().time_since_epoch())
+                    .count();
+    i = fpsdif;
+    while (--i) {
+      video_capture.grab();
+    }
+
+    video_capture >> original_frame;
+    if (original_frame.empty())
+      break;
+
+    cv::cvtColor(original_frame, grayscaled_frame, cv::COLOR_BGR2GRAY);
+    cv::resize(grayscaled_frame, grayscaled_resized_frame,
+               cv::Size(screen_width, screen_height), 0, 0, cv::INTER_LINEAR);
+
+    for (int x = 0; x < screen_height; ++x) {
+      for (int y = 0; y < screen_width; ++y) {
+        mvaddch(x, y,
+                get_ASCII_from_pixel(grayscaled_resized_frame.at<uchar>(x, y)));
+      }
+    }
+
+    refresh();
+
+    endtime = std::chrono::duration_cast<std::chrono::milliseconds>(
+                  std::chrono::system_clock::now().time_since_epoch())
+                  .count();
+
+    framedrawtime = endtime - starttime;
+
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(fpsdif * frame_duration - framedrawtime));
+  }
+
+  endwin();
+
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ConsoleVideo
 IMAGE PROCESSING IN C++ USING THE OpenCV LIBRARY
+
+Download openCV (https://www.geeksforgeeks.org/how-to-install-opencv-in-c-on-linux/)
+After open this folder => 1)cmake . 2)make 3)./CVMediaPlayer
